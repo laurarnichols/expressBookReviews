964e00bb37a39425c17ab7548db4aa0e4465696e
Laura Nichols <laurarnichols5@gmail.com>
2024-12-31 10:04:17 -0500

    Add route to delete review made by given user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,7 +33,7 @@ regd_users.post("/login", (req,res) => {
     req.session.authorization = {
       accessToken, username
     };
-    return res.status(200).send("User successfully logged in");
+    return res.status(200).json({ message: "User successfully logged in"});
   } else {
     return res.status(208).json({ message: "Invalid Login. Check username and password" });
   }
@@ -50,7 +50,27 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
     books[isbn].reviews[username] = req.body.review;
 
-    return res.status(200).send("Review successfully updated");
+    return res.status(200).json({message: "Review successfully updated"});
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  let isbn = req.params.isbn;
+
+  if(!books[isbn]) {
+    return res.status(404).json({message: "ISBN does not exist in database"});
+  } else {
+    let username = req.user.username;
+
+    if(books[isbn].reviews[username]) {
+        delete books[isbn].reviews[username];
+
+        return res.status(200).json({message: "Review successfully deleted"});
+    } else {
+        return res.status(404).json({message: `No review for ISBN ${isbn} found for user ${username}`});
+    }
+
+    
   }
 });
 
