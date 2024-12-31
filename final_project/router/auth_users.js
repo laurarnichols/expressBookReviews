9427abb869933cdcd8caf2102e8cd8bf4fb8c809
Laura Nichols <laurarnichols5@gmail.com>
2024-12-30 22:11:20 -0500

    Add user authentication and review updates

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,48 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  return validusers.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      { username }, 
+      'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken, username
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+
+  if(!books[isbn]) {
+    return res.status(404).json({message: "ISBN does not exist in database"});
+  } else {
+    let username = req.user.username;
+
+    books[isbn].reviews[username] = req.body.review;
+
+    return res.status(200).send("Review successfully updated");
+  }
 });
 
 module.exports.authenticated = regd_users;
